56327c98d49e3deabc0ccbf190288865110e6b1d
Julius.Mazionis <julius.mazionis@gmail.com>
2012-03-09 15:25:42 +0200

    Added some new classes

---
+++ VirtualMachineEmulator/Memory.cs
@@ -0,0 +1,36 @@
+﻿namespace VirtualMachineEmulator
+{
+    class Memory
+    {
+        private Word[,] words;
+
+        public Memory(int blockCount, int wordCount)
+        {
+            this.words = new Word[blockCount, wordCount];
+            for (int i = 0; i < this.words.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.words.GetLength(1); j++)
+                {
+                    this.words[i, j] = new Word();
+                }
+            }
+        }
+
+        public int BlockCount 
+        { 
+            get { return this.words.GetLength(0); }
+        }
+
+        public int WordCount
+        {
+            get { return this.words.GetLength(1); }
+        }
+
+        public Word this[int block, int word]
+        {
+            set { words[block, word] = value; }
+            get { return words[block, word]; }
+        }
+        
+    }
+}
