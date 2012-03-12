768ad0d894ca3501d697ecfe8a514ab10c1d9b0a
Julius.Mazionis <julius.mazionis@gmail.com>
2012-03-13 01:55:47 +0200

    Done and tested most of cpu commands

--- VirtualMachineEmulator/Cpu.cs
+++ VirtualMachineEmulator/Cpu.cs
@@ -8,7 +8,7 @@ namespace VirtualMachineEmulator
     public class Cpu
     {
         private Memory memory;
-        public Word PC { get; set; }
+        public short PC { get; set; }
         public Word AX { get; set; }
         public Word CX { get; set; }
         public byte SF { get; set; }
@@ -16,16 +16,42 @@ namespace VirtualMachineEmulator
         public Cpu(Memory memory)
         {
             this.memory = memory;
-            this.PC = new Word(0);
+            this.PC = 0;
             this.AX = new Word(0);
             this.CX = new Word(0);
             this.SF = 0;
         }
 
+        public bool ExecuteNext()
+        {
+            int block = this.PC / 16;
+            int word = this.PC % 16;
+            if (!(this.memory[block, word].Value == "END"))
+            {
+                this.ExecuteCommand(this.memory[block, word]);
+                if (!(this.memory[block, word].Value[0] == 'J'))
+                    this.PC++;
+                return true;
+            }
+            return false;
+        }
+
+        public void RunTask()
+        {
+            while (this.ExecuteNext())
+            {    
+            }
+        }
+
         public void ExecuteCommand(Word word)
         {
             string command = word.Value.Substring(0, 2);
-            string operand = word.Value.Substring(2, 2);
+            string operand = "";
+            if (word.Value.Length != 3)
+            {
+                operand = word.Value.Substring(2, word.Value.Length - 2);
+            }
+           
             switch (command)
             {
                 case "AD":
@@ -33,7 +59,7 @@ namespace VirtualMachineEmulator
                         this.AX = this.AX + this.CX;
                         break;
                     }
-                case "SB":
+                case "SU":
                     {
                         this.AX = this.AX - this.CX;
                         break;
@@ -44,63 +70,72 @@ namespace VirtualMachineEmulator
                             this.SF = 0;
                         if (this.AX < this.CX)
                             this.SF = 2;
-                        if (this.AX == this.CX)
+                        if (this.AX.ToHex(this.AX.Value) == this.CX.ToHex(this.CX.Value))
                             this.SF = 1;
                         break;
                     }
                 case "SA":
                     {
+                        this.memory[Convert.ToInt32(operand[0].ToString(), 16), Convert.ToInt32(operand[1].ToString(), 16)].Value = this.AX.Value;
                         break;
                     }
                 case "LA":
                     {
+                        this.AX.Value = this.memory[Convert.ToInt32(operand[0].ToString(), 16), Convert.ToInt32(operand[1].ToString(), 16)].Value;
                         break;
                     }
                 case "SC":
                     {
+                        this.memory[Convert.ToInt32(operand[0].ToString(), 16), Convert.ToInt32(operand[1].ToString(), 16)].Value = this.CX.Value;
                         break;
                     }
                 case "LC":
                     {
-                        break;
-                    }
-                case "SG":
-                    {
-                        break;
-                    }
-                case "LG":
-                    {
+                        this.CX.Value = this.memory[Convert.ToInt32(operand[0].ToString(), 16), Convert.ToInt32(operand[1].ToString(), 16)].Value;
                         break;
                     }
                 case "JM":
                     {
+                        this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
                 case "JE":
                     {
+                        if (this.SF == 1)
+                            this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
                 case "JN":
                     {
+                        if (this.SF != 1)
+                            this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
                 case "JA":
                     {
+                        if (this.SF == 0)
+                            this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
                 case "JB":
                     {
+                        if (this.SF == 2)
+                            this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
                 case "JG":
                     {
+                        if (this.SF == 0 || this.SF == 1)
+                            this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
                 case "JL":
                     {
+                        if (this.SF == 1 || this.SF == 2)
+                            this.PC = Convert.ToInt16(operand, 16);
                         break;
                     }
-                case "GD":
+                case "GD": //TO DO: everything
                     {
                         break;
                     }
@@ -112,7 +147,6 @@ namespace VirtualMachineEmulator
                     {
                         break;
                     }
-
             }
         }
     }
